2199162f8dac4caf6edf62b2bdb9d3cc824629a3
Kostyantyn Kyyashko <k.konstantin.n@gmail.com>
2018-01-13 15:34:08 +0300

    readme

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -19,19 +19,6 @@ class SiteController extends Controller
     public $layout = 'redirect';
 
     /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
-    }
-
-    /**
-     * Displays homepage.
      *
      * @return string
      */
@@ -40,32 +27,5 @@ class SiteController extends Controller
     {
         return $this->redirect('/gmonitor/');
     }
-
-    public function actionAsin()
-    {
-        $asin = 'B000FFR5KI';
-        $parser = new \app\components\Parser();
-        $product = new \app\components\Product($parser);
-//        $cookies = $parser->web_page_get($product->root_url)['cookies'];
-//        sleep(1);
-        $url = $product->url_generate($asin);$cookies='';
-        $html = $parser->web_page_get($url, $cookies)['content']; echo $html;
-        $product_fields = array_keys($product->fields());
-        $out = [];
-        foreach ($product_fields as $key) {
-            @$content = $product->get_parsed_data($key, $html);
-            $out[$key] = $content;
-        }
-        yii::$app->debig->dump($out['customer_reviews_link']);
-    }
-
-    public function actionProxy()
-    {
-        $proxy = new Proxy();
-        $proxy->proxy_list_by_file();
-        yii::$app->debig->view($proxy->proxy_list());
-    }
-
-
 
 }
